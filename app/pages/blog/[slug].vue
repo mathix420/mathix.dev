d2809bb2ec6fbf5f3700f5bf7ed5fde2799d3aaa
Arnaud Gissinger <agissing@student.42.fr>
2024-10-25 17:05:24 +0200

    refactor: Update blog and improve warning message

--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -3,7 +3,7 @@
     <AppHeader />
     <main class="prose xl:prose-xl prose-invert prose-a:no-underline prose-a:text-accent prose-headings:text-white prose-a:prose-headings:text-white container mx-auto px-2 py-10 xl:py-20">
       <p class="text-center text-lg font-bold text-gray-500 dark:text-gray-400 ">
-        🚨 My blog section is in alpha, come back later if you want polished posts and nicer UI!
+        🚨 WIP, come back later if you want polished blog posts and nicer UI!
       </p>
 
       <ContentDoc class="break-words" />
