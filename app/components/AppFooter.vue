4b1c859f5fe664df1673609f9e6221a6df9d493f
Arnaud Gissinger <agissing@student.42.fr>
2024-11-08 11:40:17 +0100

    refactor: Update Signature size in AppFooter.vue

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="mt-32 flex flex-col items-center gap-5 text-white/25 transition-colors duration-1000 hover:text-white">
-    <Signature class="h-14 w-fit sm:h-28" />
+    <Signature class="h-14 w-fit sm:h-20" />
     <p class="text-center">
       By Arnaud Gissinger in 2024.
     </p>
