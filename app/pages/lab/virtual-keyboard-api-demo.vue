47aeacc77f09ffeff2070d5c31442829505eb1bb
Arnaud Gissinger <agissing@student.42.fr>
2024-12-10 18:16:44 +0100

    refactor: latest blog article

--- app/pages/lab/virtual-keyboard-api-demo.vue
+++ app/pages/lab/virtual-keyboard-api-demo.vue
@@ -39,7 +39,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="h-svh p-5 sm:p-10">
+  <div class="min-h-svh p-5 sm:p-10">
     <h1 class="mb-3 text-2xl font-black sm:text-3xl">
       Virtual Keyboard API Demo (+ iOS workaround)
     </h1>
@@ -49,6 +49,43 @@ onMounted(() => {
       class="w-full rounded-md border-none text-black"
       type="text"
     >
+
+    <pre class="my-10 overflow-y-auto">
+const { isIos } = useDevice();
+const dock = ref&lt;HTMLElement | null&gt;(null);
+
+onMounted(() => {
+  // For chromium
+  if ("virtualKeyboard" in navigator) {
+    navigator.virtualKeyboard.overlaysContent = true;
+  }
+  // For iOS
+  if (isIos && window.visualViewport) {
+    const vv = window.visualViewport;
+
+    function fixPosition() {
+      const ih = window.innerHeight;
+
+      if (dock.value) {
+        dock.value.style.bottom = `${Math.max(0, ih - vv.height - vv.offsetTop)}px`;
+      }
+    }
+
+    vv.addEventListener("resize", fixPosition);
+    vv.addEventListener("scroll", fixPosition);
+    document.addEventListener("gesturechange", fixPosition);
+    document.addEventListener("focusout", fixPosition);
+
+    fixPosition();
+
+    onBeforeUnmount(() => {
+      vv.removeEventListener("resize", fixPosition);
+      vv.removeEventListener("scroll", fixPosition);
+      document.removeEventListener("gesturechange", fixPosition);
+      document.removeEventListener("focusout", fixPosition);
+    });
+  }
+});</pre>
 
     <div
       ref="dock"
