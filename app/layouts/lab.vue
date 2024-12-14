9cf8737d48b1a29cd0ae71ca8875596752695504
Arnaud Gissinger <agissing@student.42.fr>
2024-12-14 15:59:45 +0100

    feat: add OgImage component setup and improve layout styling in lab.vue

--- app/layouts/lab.vue
+++ app/layouts/lab.vue
@@ -1,15 +1,16 @@
+<script setup lang="ts">
+defineOgImageComponent("Lab");
+</script>
+
 <template>
   <main>
     <slot />
-    <OgImage
-      component="OgImageLab"
-    />
   </main>
 </template>
 
 <style lang="css" scoped>
   main {
-    background-color:#1173ff;
+    background-color: #1173ff;
     background-image: linear-gradient(rgba(255,255,255,0.1) 1px, transparent 1px),
       linear-gradient(90deg, rgba(255,255,255,0.1) 1px, transparent 1px),
       linear-gradient(rgba(255,255,255,0.05) 1px, transparent 1px),
