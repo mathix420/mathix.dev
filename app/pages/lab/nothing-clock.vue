8e41dcef45cf34f1cc8eab6039228288333ade20
Arnaud Gissinger <agissing@student.42.fr>
2024-12-15 18:05:46 +0100

    feat: enhance nothing-clock functionality and update styles

--- app/pages/lab/nothing-clock.vue
+++ app/pages/lab/nothing-clock.vue
@@ -8,19 +8,24 @@ const minutesAngle = ref(0);
 const hoursAngle = ref(0);
 
 onMounted(() => {
-  setInterval(() => {
-    const currentTime = performance.timeOrigin + performance.now();
-    secondsAngle.value = ((currentTime / 1000) % 60 * 6);
-    minutesAngle.value = (currentTime / 1000 / 60) % 60 * 6;
-    hoursAngle.value = (currentTime / 1000 / 60 / 60) % 12 * 30;
-  }, 50);
+  setTimeout(() => {
+    setInterval(() => {
+      const currentTime = performance.timeOrigin + performance.now();
+      secondsAngle.value = ((currentTime / 1000) % 60 * 6);
+      minutesAngle.value = (currentTime / 1000 / 60 + 35) % 60 * 6;
+      hoursAngle.value = (currentTime / 1000 / 60 / 60) % 12 * 30;
+    }, 50);
+  }, 200);
 });
 </script>
 
 <template>
   <main class="flex h-svh flex-col items-center justify-center">
-    <div class="rounded-full bg-[#1173ff]/10 p-10 backdrop-blur-[1px]">
-      <div class="relative z-10 h-40 w-96 overflow-hidden rounded-full p-7">
+    <p class="font-mono">
+      LAB 00002
+    </p>
+    <div class="relative">
+      <div class="relative z-10 h-[37.5vw] w-[90vw] overflow-hidden rounded-full p-7 sm:h-40 sm:w-96">
         <div
           v-for="angle in mainAngles"
           :key="angle"
@@ -36,8 +41,8 @@ onMounted(() => {
         <div class="size-full rounded-full bg-[#1173ff]">
           <div
             :style="{ '--tw-rotate': `${hoursAngle}deg` }"
-            :class="hoursAngle > 359.95 || hoursAngle < 0.15 ? '' : 'transition-transform'"
-            class="absolute bottom-1/2 right-1/2 h-10 w-[6px] origin-bottom translate-x-1/2 rounded-full bg-white"
+            :class="hoursAngle > 359.91 || hoursAngle < 0.11 ? '' : 'transition-transform'"
+            class="absolute bottom-1/2 right-1/2 h-1/4 w-[6px] origin-bottom translate-x-1/2 rounded-full bg-white"
           >
             <div
               class="absolute -bottom-6 h-6 w-[6px] rounded-full bg-white"
@@ -45,8 +50,8 @@ onMounted(() => {
           </div>
           <div
             :style="{ '--tw-rotate': `${minutesAngle}deg` }"
-            :class="minutesAngle > 359.95 || minutesAngle < 0.15 ? '' : 'transition-transform'"
-            class="absolute bottom-1/2 right-1/2 h-20 w-[4px] origin-bottom translate-x-1/2 rounded-full bg-white"
+            :class="minutesAngle > 359.91 || minutesAngle < 0.11 ? '' : 'transition-transform'"
+            class="absolute bottom-1/2 right-1/2 h-1/2 w-[4px] origin-bottom translate-x-1/2 rounded-full bg-white"
           >
             <div
               class="absolute bottom-0 left-1/2 size-3 -translate-x-1/2 translate-y-1/2 rounded-full bg-white"
@@ -57,8 +62,8 @@ onMounted(() => {
           </div>
           <div
             :style="{ '--tw-rotate': `${secondsAngle}deg` }"
-            :class="secondsAngle > 359.95 || secondsAngle < 0.15 ? '' : 'transition-transform'"
-            class="absolute bottom-1/2 right-1/2 h-20 w-[2px] origin-bottom translate-x-1/2 rounded-full bg-red-500"
+            :class="secondsAngle > 359.91 || secondsAngle < 0.11 ? '' : 'transition-transform'"
+            class="absolute bottom-1/2 right-1/2 h-1/2 w-[2px] origin-bottom translate-x-1/2 rounded-full bg-red-500"
           >
             <div
               class="absolute bottom-0 left-1/2 size-2 -translate-x-1/2 translate-y-1/2 rounded-full bg-red-500"
@@ -69,6 +74,7 @@ onMounted(() => {
           </div>
         </div>
       </div>
+      <div class="absolute -inset-10 rounded-full bg-[#1173ff]/10 backdrop-blur-[1px]" />
     </div>
   </main>
 </template>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   theme: {
-    fontFamily: {
-      sans: ["Archivo", "ui-sans-serif", "system-ui", "sans-serif", "\"Apple Color Emoji\"", "\"Segoe UI Emoji\"", "\"Segoe UI Symbol\"", "\"Noto Color Emoji\""],
-    },
     extend: {
+      fontFamily: {
+        sans: ["Archivo", "ui-sans-serif", "system-ui", "sans-serif", "\"Apple Color Emoji\"", "\"Segoe UI Emoji\"", "\"Segoe UI Symbol\"", "\"Noto Color Emoji\""],
+      },
       colors: {
         crimson: "#DC143C",
         charcoal: "#0d0d0d",
